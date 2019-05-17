cfe133e680e432d2830f0676ddb2149bfeadbac9
samuel diogo <samudiogo@gmail.com>
2019-05-17 11:52:37 -0300

    extracting topnavbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,19 +6,23 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [TopNavbarComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule,
   ], exports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    TopNavbarComponent
   ]
 })
 export class CoreModule { }
